c6fb98ff6c718f5efa674e2cd72afe0e4288f81e
lualoman <95001406+lualoman@users.noreply.github.com>
2022-07-09 18:11:29 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -36,20 +36,24 @@ if (numeroEntero > numeroDiez && numeroEntero < numeroCincuenta) {
 
 
 /*1ER DESAFIO COMPLEMENTARIO
-for (let i = 1; i <=3; i++) {
+
+for (let i = 0; i <= 3; i++) {
     let ingresarNumero = parseInt(prompt("Ingresa un numero"));
     alert ("Hola numero"+" "+ingresarNumero);
 }
 
+
+
 let añoDeNacimiento = parseInt(prompt("Ingresa tu año de nacimiento"));
 
-while (añoDeNacimiento>2000) {
-    alert("Veras tu permitso ingresando a la consola")
-    console.log("¡Acceso denegado! Solo se permiten mayores de 21 años.");
-    añoDeNacimiento = prompt("Ingresa tu año de nacimiento");
+if(añoDeNacimiento>2001) {
+    alert("Veras tu permiso ingresando a la consola")
+    console.log("¡Acceso denegado! Solo se permiten mayores de 21 años."); 
+}else {
+    alert("Sos mayor de 21 años, ¡Acceso concedido!")
 }
 
-alert("Sos mayor de 21 años, ¡Acceso concedido!")*/
+
 
 let nombreUnico = "Homero";
 
@@ -61,4 +65,73 @@ while (nombreClub != nombreUnico) {
 }
 
 alert("Excelente...Solo se permite UN Homero");
+*/
+
+
+
+
+/*1ER DESAFIO OBLIGATORIO*/
+let edadUsuario = parseInt(prompt("Ingresa tu edad"));
+
+if(edadUsuario >= 18) {
+    alert("Bienvenid@ a ¡Coco Loco!");
+}else if(edadUsuario < 18){
+    alert("Debes tener al menos 18 años para acceder a este sitio");
+}else{
+    alert("Ingresaste un dato inválido");
+}
+
+
+
+if(edadUsuario >=18){
+    let comboFernet = 1780;
+    let comboGancia = 1420;
+    let comboCampari = 1650;
+
+    let opcion = prompt("¡Elegi tu combo de bebidas!: \n1. Fernet 1LT+ Coca 3LT = $1780 \n2. Gancia 1LT+ Sprite 3LT = $1420 \n3. Campari 1LT+ Citric 3LT = $1650 \n Presiona ESC para Cancelar");
+
+    while(opcion!="ESC"){
+        switch (opcion) {
+            case "1":
+                let compraFernet = parseInt(prompt("Ahorremos agua, ¡tomemos fernet! Ingresa la cantidad de combos que deseas comprar"));
+
+                for(i=0; 0<=i; i--){
+                let compraFinalFernet = comboFernet*compraFernet;
+                alert("Tu compra final tiene un valor de $"+compraFinalFernet);
+                }
+                break;
+
+            case "2":
+                let compraFinalGancia = true;
+                let compraGancia = parseInt(prompt("¡Que elegancia la del Gancia! Ingresa la cantidad de combos que deseas comprar"));
+
+                for(i=0; 0<=i; i--){
+                compraFinalGancia = comboGancia*compraGancia;
+                alert("Tu compra final tiene un valor de $"+compraFinalGancia);
+                compraFinalGancia = false;
+                }
+                break;
+
+            case "3":
+                let compraFinalCampari = true;
+                let compraCampari = parseInt(prompt("¡Siga el party con el Campari! Ingresa la cantidad de combos que deseas comprar"));
+
+                for(i=0; 0<=i; i--){
+                compraFinalCampari = comboCampari*compraCampari;
+                alert("Tu compra final tiene un valor de $"+compraFinalCampari);
+                compraFinalCampari = false;
+                }
+                break;
+
+            default:
+                alert("elegiste una opcion invalida");
+                break;
+        }
+        break;
+    }
+    alert("¡Gracias por tu visita!");
+}else{
+    alert("Prohibida la venta de bebidas alcoholicas a menores de 18 años");
+}
+
 
