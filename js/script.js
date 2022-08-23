8059089856b5b62c3c4161851fba767ea2e4b2a3
lualoman <95001406+lualoman@users.noreply.github.com>
2022-08-23 02:26:03 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,137 +1,259 @@
-/*HANDS ON 1
-let nombreUsuario = prompt('Escribí tu nombre');
-let alturaUsuario = parseFloat (prompt('Ingresa tu altura'));
 
-alert('Tu nombre es '+nombreUsuario+' y tu altura es '+alturaUsuario+' mts')*/
+let carrito = []
 
-
-
-/*HANDS ON 2
-Ejemplo 1:
-let mayorQueMil = 1000;
-let numeroEntero = parseInt (prompt('Ingresa un numero entero'));
-
-if (numeroEntero > mayorQueMil) {
-    alert('Tu numero ingresado es MAYOR a 1000');
-}else if(numeroEntero == mayorQueMil){
-    alert('Tu numero ingresado es IGUAL a 1000');
-}else{
-    alert('Tu numero ingresado es MENOR a 1000');
-}
+const contenedor = document.getElementById("contenedor"),
+    contenedorCarrito = document.getElementById("contenedorCarrito"),
+    precioFinal = document.getElementById("precioFinal"),
+    vaciarCarrito = document.getElementById("vaciarCarrito"),
+    btnDelivery = document.getElementById("btnDelivery"),
+    cliente = document.getElementById("cliente"),
+    direccion = document.getElementById("direccion"),
+    inputForm = document.getElementById("inputForm"),
+    finalizar = document.getElementById("finalizar");
 
 
-Ejemplo 2:
-let numeroDiez = 10;
-let numeroCincuenta = 50;
-let numeroEntero = parseInt (prompt('Ingresa un numero entero entre 10 y 50'));
+//PEDIR CONFIRMACION DE EDAD
 
-if (numeroEntero > numeroDiez && numeroEntero < numeroCincuenta) {
-    alert('Tu numero ingresado esta entre 10 y 50');
-}else{
-    alert('Tu numero ingresado es NO se encuentra entre 10 y 50');
-}*/
+//Con estás dos líneas se revisa PRIMERO el storage, y sólo si no tiene el booleano de esMayor se confirma que sea mayor de edad. Si no, directamente se muestra la página.
+
+const edadGuardada = JSON.parse(localStorage.getItem("esMayor"));
+edadGuardada ? mostrarPagina() : confirmarUsuarioMayor();
 
 
+function confirmarUsuarioMayor() {
+    const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+            confirmButton: 'btn btn-success',
+            cancelButton: 'btn btn-danger'
+        },
+        buttonsStyling: false
+    })
 
-
-
-/*1ER DESAFIO COMPLEMENTARIO
-
-for (let i = 0; i <= 3; i++) {
-    let ingresarNumero = parseInt(prompt("Ingresa un numero"));
-    alert ("Hola numero"+" "+ingresarNumero);
-}
-
-
-
-let añoDeNacimiento = parseInt(prompt("Ingresa tu año de nacimiento"));
-
-if(añoDeNacimiento>2001) {
-    alert("Veras tu permiso ingresando a la consola")
-    console.log("¡Acceso denegado! Solo se permiten mayores de 21 años."); 
-}else {
-    alert("Sos mayor de 21 años, ¡Acceso concedido!")
-}
-
-
-
-let nombreUnico = "Homero";
-
-let nombreClub = prompt("¿Que nombre de niño era aceptado solo una vez en el Club de los NO Homeros?")
-
-while (nombreClub != nombreUnico) {
-    alert("Nombre incorrecto, ¡Simpsonizate!");
-    nombreClub = prompt("¿Que nombre de niño era aceptado solo una vez en el Club de los NO Homeros?");
-}
-
-alert("Excelente...Solo se permite UN Homero");
-*/
-
-
-
-
-/*1ER DESAFIO OBLIGATORIO*/
-let edadUsuario = parseInt(prompt("Ingresa tu edad"));
-
-if(edadUsuario >= 18) {
-    alert("Bienvenid@ a ¡Coco Loco!");
-}else if(edadUsuario < 18){
-    alert("Debes tener al menos 18 años para acceder a este sitio");
-}else{
-    alert("Ingresaste un dato inválido");
-}
-
-
-
-if(edadUsuario >=18){
-    let comboFernet = 1780;
-    let comboGancia = 1420;
-    let comboCampari = 1650;
-
-    let opcion = prompt("¡Elegi tu combo de bebidas!: \n1. Fernet 1LT+ Coca 3LT = $1780 \n2. Gancia 1LT+ Sprite 3LT = $1420 \n3. Campari 1LT+ Citric 3LT = $1650 \n Presiona ESC para Cancelar");
-
-    while(opcion!="ESC"){
-        switch (opcion) {
-            case "1":
-                let compraFernet = parseInt(prompt("Ahorremos agua, ¡tomemos fernet! Ingresa la cantidad de combos que deseas comprar"));
-
-                for(i=0; 0<=i; i--){
-                let compraFinalFernet = comboFernet*compraFernet;
-                alert("Tu compra final tiene un valor de $"+compraFinalFernet);
-                }
-                break;
-
-            case "2":
-                let compraFinalGancia = true;
-                let compraGancia = parseInt(prompt("¡Que elegancia la del Gancia! Ingresa la cantidad de combos que deseas comprar"));
-
-                for(i=0; 0<=i; i--){
-                compraFinalGancia = comboGancia*compraGancia;
-                alert("Tu compra final tiene un valor de $"+compraFinalGancia);
-                compraFinalGancia = false;
-                }
-                break;
-
-            case "3":
-                let compraFinalCampari = true;
-                let compraCampari = parseInt(prompt("¡Siga el party con el Campari! Ingresa la cantidad de combos que deseas comprar"));
-
-                for(i=0; 0<=i; i--){
-                compraFinalCampari = comboCampari*compraCampari;
-                alert("Tu compra final tiene un valor de $"+compraFinalCampari);
-                compraFinalCampari = false;
-                }
-                break;
-
-            default:
-                alert("elegiste una opcion invalida");
-                break;
+    swalWithBootstrapButtons.fire({
+        title: '¿Eres mayor de 18 años?',
+        text: "El pedido no se entregará a menores de edad",
+        color: "white",
+        icon: 'warning',
+        background: "#141414",
+        confirmButtonText: 'Si, soy mayor',
+        cancelButtonText: 'Cancelar',
+        showCancelButton: true,
+        reverseButtons: true
+    }).then((result) => {
+        if (result.isConfirmed) {
+            guardarEdad(localStorage);
+            swalWithBootstrapButtons.fire(
+                '¡Confirmado!',
+                'Solicitaremos tu DNI al momento de la entrega',
+                'success'
+            );
+            mostrarPagina();
+        } else if (result.dismiss === Swal.DismissReason.cancel) {
+            swalWithBootstrapButtons.fire(
+                'Cancelado',
+                'Regresa cuando seas mayor de edad :)',
+                'error'
+            );
         }
-        break;
-    }
-    alert("¡Gracias por tu visita!");
-}else{
-    alert("Prohibida la venta de bebidas alcoholicas a menores de 18 años");
+    })
+}
+
+function guardarEdad(storage) {
+    storage.setItem("esMayor", JSON.stringify(true));
+    //Si usás un parámetro, no hace falta que hagas el if, porque se va a guardar en el storage correspondiente
+}
+
+//---------------------------------------------------------
+//---------------------------------------------------------
+function buscarProducto(array, idProducto) {
+    return array.find(item => item.id == idProducto);
+}
+
+function obtenerIDdeBoton(cadena, nombre) {
+    return cadena.replace(nombre, "");
 }
 
 
+//CREACION DE TARJETAS
+function mostrarPagina() {
+
+    //Acá hacemos el fetch, y creamos las tarjetas con la info traída desde el json
+    fetch("data.json")
+        .then((response) => response.json())
+        .then((data) => {
+            data.forEach(combo => {
+                let tarjeta = `<div class="col-md-6 col-xl-4 scale" id="tarjetas">
+                <div class="card-group">
+                <div class="card-body tarjeta">
+                    <img src=${combo.imagen} class="card-img-top" alt="imagen de combo">
+                    <h5 class="card-title" id="nombre">
+                        <hr>${combo.nombre} creado con fetch
+                    </h5>
+                    <p class="card-text">${combo.descripcion}</p>
+                    <p class="card-price" id="precio">$ ${combo.precio}</p>
+                    <div class="d-grid gap-2">
+                        <button type="submit" class="btn btn__comprar btn-dark" id="agregarItem${combo.id}">¡COMPRAR!</button>
+                    </div>
+                </div>
+            </div>
+        </div>`
+                    ;
+
+                contenedor.innerHTML += tarjeta;
+                let btnsComprar = document.querySelectorAll(".btn__comprar");
+
+                btnsComprar.forEach(btn => {
+                    btn.addEventListener("click", () => {
+                        let id = obtenerIDdeBoton(btn.id, "agregarItem");
+                        console.log(id);
+
+                        let producto = buscarProducto(data, id);
+                        console.log(producto);
+                        carrito.push(producto);
+                        renderizarCarrito();
+                    })
+                })
+            })
+        })
+
+}
+
+//CREACION DE CARRITO y APLICACION DE BTN ELIMINAR
+const renderizarCarrito = () => {
+    const contenedorCarrito = document.querySelector("#contenedorCarrito")
+    contenedorCarrito.innerHTML = ""
+
+    carrito.forEach((producto) => {
+        const div = document.createElement("div")
+        div.innerHTML = `
+                <div class="row">
+                <div class="col"><p>${producto.nombre}</p></div>
+                <div class="col"><p>$ ${producto.precio}</p></div>
+                <div class="col"><span id="cantidad">${producto.cantidad}</div>
+                <div class="col"><button class="btn__eliminar" id="eliminarItem${producto.id}">ELIMINAR</button></div>
+                </div>
+                `
+        contenedorCarrito.appendChild(div)
+    })
+
+
+    let btnsEliminar = document.querySelectorAll('.btn__eliminar');
+    btnsEliminar.forEach(btn => {
+        btn.addEventListener('click', () => {
+            let id = obtenerIDdeBoton(btn.id, 'eliminarItem');
+            let producto = buscarProducto(carrito, id);
+            const indice = carrito.indexOf(producto);
+            carrito.splice(indice, 1);
+            console.log("Se ha eliminado un combo: ", id)
+
+            renderizarCarrito();
+
+            if (carrito.length === 0) {
+                precioFinal.innerText = 0;
+            }
+        })
+
+        precioFinal.innerText = carrito.reduce((acc, combo) => acc + combo.precio, 0)
+
+    })
+}
+
+
+function limpiarCarrito(){
+    carrito.length = 0;
+    precioFinal.innerText = 0;
+    renderizarCarrito();
+    console.log("Se ha vaciado el carrito")
+}
+
+vaciarCarrito.addEventListener("click", () => {
+    limpiarCarrito();
+})
+
+
+
+btnDelivery.addEventListener("click", () => {
+    window.scrollTo({ top: 1500, behavior: "smooth" });
+})
+
+
+
+//---------------------------------------------------------------ENVIO DE FORMULARIO
+envioForm()
+function envioForm() {
+    document.addEventListener("DOMContentLoaded", fnzDisabled)
+
+    contenedorCarrito.addEventListener("blur", validacionForm)
+    precioFinal.addEventListener("blur", validacionForm)
+    cliente.addEventListener("blur", validacionForm)
+    direccion.addEventListener("blur", validacionForm)
+
+    inputForm.addEventListener("click", enviarForm)
+}
+
+function fnzDisabled() {
+    finalizar.disabled = true
+}
+
+function validacionForm(e) {
+
+    if (e.target.value.length > 0) {
+        const invalido = document.querySelector("p.invalido")
+        if (invalido) {
+            invalido.remove();
+        }
+
+        e.target.classList.add("validado")
+        e.target.classList.remove("obligatorio")
+
+        if (cliente.value !== "" && direccion.value !== "" && carrito.length !== 0) {
+            finalizar.disabled = false;
+        } else if (carrito.length === 0) {
+            msjObligatorio("El carrito esta vacio. Elige un producto y reingresa tus datos.")
+        }
+
+    } else {
+        e.target.classList.add("obligatorio")
+        e.target.classList.remove("validado")
+        msjObligatorio("Completa los campos obligatorios")
+    }
+
+}
+
+function msjObligatorio(mensaje) {
+    const msjError = document.createElement("p")
+    msjError.textContent = mensaje
+    msjError.classList.add("msjObligatorio", "invalido")
+
+    const error = document.querySelectorAll(".invalido")
+    if (error.length === 0) {
+        inputForm.appendChild(msjError)
+    }
+}
+
+function enviarForm(e) {
+    e.preventDefault();
+
+    const finalizar = document.getElementById("finalizar");
+
+    finalizar.addEventListener("click", () => {
+        Swal.fire({
+            position: "center",
+            title: "Tu pedido está en camino",
+            text: "¡Gracias por elegirnos!",
+            icon: "success",
+            iconColor: "#198754",
+            confirmButtonText: "Ok",
+            confirmButtonColor: "#198754",
+            background: "#141414",
+            color: "white",
+        })
+        limpiarForm();
+        limpiarCarrito();
+
+    })
+}
+
+function limpiarForm() {
+    inputForm.reset()
+}
